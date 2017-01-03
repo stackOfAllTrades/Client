156ff1fa2052d6453975c38a375c583714dbd066
Matt Seaton <matt.r.seaton@gmail.com>
2017-01-03 15:38:15 -0700

    populating with dummy data, description included

--- list/app.js
+++ list/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var eventURL = 'https://stack-of-all-trade.herokuapp.com/events';
 
   $.get(eventURL, function(data){
-    for(var i = 0; i < data.length; i++){
+    for(var i = 0; i < 8; i++){
       var cardId = data[i].id;
       var $id = $('<p class="hidden">' + cardId + '</p>');
       var $card = $('<div class="card" + id="card' + (i+1) + '"></div>');
@@ -24,37 +24,22 @@ $(document).ready(function(){
       var thisId = parseInt($(this).children(':first').text()-1);
       $('main').empty();
       var $bigDiv = $('<div class="big-div"></>');
+      var $backLink = $('<a class=back-link href="index.html">Back to List</a>');
       var $bigName = $('<h3 class="big-title">' + data[thisId].eventName + '</h3>');
       var $bigDate = $('<p class="big-date">' + data[thisId].date + ' at ' + data[thisId].time + '</p>');
       var $description = $('<p class="big-description">' + data[thisId].description + '</p>');
-      var $bigLink = $('<a class="big-link" href="' + data[thisId].link + '">Click this link for further details</a>');
-
-      $('main').append($bigDiv);
+      var $bigLink = $('<a class="big-link" href="' + data[thisId].eventLink + '">Click this link for further details</a>');
+      var $main2 = $('<div class="main2"></div>');
+      $('main').remove();
+      $('body').append($main2);
+      $('.main2').append($bigDiv);
+      $($bigDiv).append($backLink);
       $($bigDiv).append($bigName);
       $($bigDiv).append($bigDate);
       $($bigDiv).append($description);
       $($bigDiv).append($bigLink);
-
-
-      // for (var j = 0; j < data.length; j++) {
-      //   if (thisId === i) {
-      //     var $id = $('<p class="hidden">' + cardId + '</p>');
-      //     var $bigCard = $('<div class="big-div"></div>');
-      //     var $h3 = $('<h3>' + data[i].eventName + '</h3>');
-      //     var $p1 = $('<p class="location">' + data[i].date + 'at' + data[i].time + '</p>');
-      //     var $p2 = $('<p class="description">' + data[i].description + '</p>');
-      //     var $p3 = $('<a class="thisLink" href="' + data[i].link + '"> The Link</p>');
-      //     $('main').empty();
-      //     $('main').append($bigCard);
-      //     $($bigCard).append($h3);
-      //     $($bigCard).append($p1);
-      //     $($bigCard).append($p2);
-      //     $($bigCard).append($p3);
-      //   }
-      // }
+      });
     });
-  });
-
 });
 
 
