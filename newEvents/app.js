688d7fb9aad14085664885d3822d695b8085f5dd
Matt Seaton <matt.r.seaton@gmail.com>
2017-01-05 12:36:41 -0700

    Need to add Mark's Logic to events page

---
+++ newEvents/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+  var eventURL = 'https://stack-of-all-trade.herokuapp.com/events';
+
+  $.get(eventURL, function(data){
+    console.log(data);
+    for(var i = 0; i < data.length; i++){
+      var cardId = i;
+      var $id = $('<p class="hidden">' + cardId + '</p>');
+      var $card = $('<div class="card" + id="card' + (i+1) + '"></div>');
+      if (cardId === 0) {
+        $card.attr('id', 'card1');
+      }
+      var $h3 = $('<h3>' + data[i].eventName + '</h3>');
+      var $p = $('<p class="date">' + data[i].date + ' at ' + data[i].time + '</p>');
+      $($card).append($id);
+      $($card).append($h3);
+      $($card).append($p);
+
+      $('.column').append($card);
+
+      // if (data[i].id % 2 === 0) {
+      //   $('#column2').append($card);
+      //
+      // } else {
+      //   $('#column1').append($card);
+      // }
+    }
+    $('.card').click(function(){
+      var thisId = parseInt($(this).children(':first').text());
+      $('main').empty();
+      var $id = $('<p>'+thisId+'</p>');
+      var $bigDiv = $('<div class="big-div"></>');
+      var $backLink = $('<a class=back-link href="index.html">Back to List</a>');
+      var $bigName = $('<h3 class="big-title">' + data[thisId].eventName + '</h3>');
+      var $bigDate = $('<p class="big-date">' + data[thisId].date + ' at ' + data[thisId].time + '</p>');
+      var $description = $('<p class="big-description">' + data[thisId].description + '</p>');
+      var $bigLink = $('<a class="big-link" href="' + data[thisId].eventLink + '">Click this link for further details</a>');
+      var $main2 = $('<div class="main2"></div>');
+      // var $price = $('<p class="price"></p>');
+
+      $('main').append($bigDiv);
+      $($bigDiv).append($id);
+      $($bigDiv).append($backLink);
+      $($bigDiv).append($bigName);
+      $($bigDiv).append($bigDate);
+      $($bigDiv).append($description);
+      $($bigDiv).append($bigLink);
+      // $($bigDiv).append($price);
+      });
+    });
+});
+
+
+function parseId(string){
+  var noLetter = string.replace(/[a-z]/g, '');
+  console.log(noLetter);
+}
