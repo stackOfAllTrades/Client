cd5b34901bdca728754a59aefa98dadb243b89fa
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2017-01-08 16:05:24 -0700

    added refresh for error in scraping

--- scripts/user.js
+++ scripts/user.js
@@ -46,8 +46,8 @@
              console.log(error);
              console.log(error.status);
              //  if (res.status === 500) {
-             // alert("Sorry... our bad. Reloading the page.");
-             // window.location.reload();
+             alert("Sorry... our bad. Reloading the page.");
+             window.location.reload();
              //  }
          });
 
