ebdc67f5ca1e0c3046e8d873684aad2c42d77335
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2017-01-08 18:24:59 -0700

    ready for fireabse final deploy

--- scripts/guest.js
+++ scripts/guest.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
             for (var i = 0; i < categoryArray.length; i++) {
                 var categoryName = categoryArray[i].name;
                 var categoryId = categoryArray[i].id;
-                var $listItem = $(`<div class="list-group-item" id="category-${categoryId}" val="${categoryId}">${categoryName}</div>`);
+                var $listItem = $(`<div class="list-group-item category-filter" id="category-${categoryId}" val="${categoryId}">${categoryName}</div>`);
                 $('#listTwo').append($listItem);
                 createClickHandler(categoryId);
             }
@@ -45,77 +45,22 @@ $(document).ready(function() {
             }
         })
         .catch((error) => {
-            // if (res.status === 500) {
-                alert("Sorry... our bad. Reloading the page.");
-                window.location.reload();
-            // }
+            alert("Sorry... our bad. Reloading the page.");
+            window.location.reload();
         })
 
 
-    // $('#subButton').click(function() {
-    //
-    //     for (var i = 0; i < eventArray.length; i++) {
-    //
-    //         var thisEvent = eventArray[i];
-    //         thisEvent = normalizeData(thisEvent);
-    //
-    //         var $id = $('<p class="hidden">' + i + '</p>');
-    //         var $card = $('<div class="panel panel-default" + id="card' + (i + 1) + '"></div>');
-    //         if (i === 0) {
-    //             $card.attr('id', 'card1');
-    //         }
-    //
-    //         var $heading = $('<div class="panel-heading"><h3 class="panel-title">' + thisEvent.event_name + '</h3></div>');
-    //         var $body = $('<div class="panel-body"">' + thisEvent.date + ' at ' + thisEvent.time + '</div>');
-    //         $($card).append($id);
-    //         $($card).append($heading);
-    //         $($card).append($body);
 
 
-            $('.panel').click(function() {
-                var thisId = parseInt($(this).children(':first').text());
-                var thisEvent = eventArray[thisId];
-                thisEvent = normalizeData(thisEvent);
-
-
-                $('main').empty();
-                var $id = $('<p class="hidden">' + thisId + '</p>');
-                var $bigDiv = $('<div class="jumbotron"></>');
-                var $flagLink = $('<div class="flag-link"></div>');
-                var $flag = $('<button type="button" class="btn btn-primary"><p class="flagger">w</p></button>');
-                var $backLink = $('<a class="btn btn-primary" id="back-button" href="index.html">Back to List</a>');
-                var $bigName = $('<h3 class="big-title">' + thisEvent.event_name + '</h3>');
-                var $bigDate = $('<p class="big-date">' + thisEvent.date + ' at ' + thisEvent.time + '</p>');
-                var $description = $('<p class="big-description">' + thisEvent.description + '</p>');
-                var $price = $('<p class="price"><small>' + thisEvent.price + "</p>");
-                var $bigLink = $('<a class="btn btn-primary btn-lg" class="link-button" href="' + thisEvent.event_link + '">Click this link for further details</a>');
-                var $address = $('<p class="address">' + thisEvent.address + '</p>');
-
-                $('main').append($bigDiv);
-                $($bigDiv).append($id);
-                $($flagLink).append($backLink);
-                $($flagLink).append($flag);
-                $($bigDiv).append($flagLink);
-                $($bigDiv).append($bigName);
-                $($bigDiv).append($bigDate);
-                $($bigDiv).append($description);
-                $($bigDiv).append($address);
-                $($bigDiv).append($price);
-                $($bigDiv).append($bigLink);
-            });
-            // });
-
-        // }
-
-    });
 });
 
+function createEventClickHandler(event) {
+    const $thisCardBtn = $(`#card-${event.id}`);
+    $thisCardBtn.click(() => {
+        window.open(event.event_link);
+    })
+}
 
-
-// function parseId(string) {
-//     var noLetter = string.replace(/[a-z]/g, '');
-//     console.log(noLetter);
-// }
 
 function createClickHandler(id) {
     $(`#category-${id}`).click(function() {
@@ -151,20 +96,19 @@ function populateEvent(thisEvent, i) {
     thisEvent = normalizeData(thisEvent);
 
 
-    var $id = $('<p class="hidden">' + i + '</p>');
-    var $card = $('<div class="panel panel-default" + id="card' + (i + 1) + '"></div>');
+    var $card = $('<div class="panel panel-default event-card" + id="card-' + thisEvent.id + '"></div>');
     if (i === 0) {
         $card.attr('id', 'card1');
     }
 
     var $heading = $('<div class="panel-heading"><h3 class="panel-title">' + thisEvent.event_name + '</h3></div>');
     var $body = $('<div class="panel-body"">' + thisEvent.date + ' at ' + thisEvent.time + '</div>');
-    $($card).append($id);
     $($card).append($heading);
     $($card).append($body);
 
     $('#listOne').append($card);
 
+    createEventClickHandler(thisEvent);
 
 
 }
@@ -188,10 +132,10 @@ function normalizeArray(array) {
 
 
 
- function createGoogleButtonHandler() {
-     $loginButton = $("#sign-in");
-     $loginButton.attr('href', `${SERVER_URL}/auth/google`);
- }
+function createGoogleButtonHandler() {
+    $loginButton = $("#sign-in");
+    $loginButton.attr('href', `${SERVER_URL}/auth/google`);
+}
 
 
 function normalizeData(event) {
